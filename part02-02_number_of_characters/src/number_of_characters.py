509f1d40991f67eae97f57d1f5b52c10e0208af7
ryor31057 <ryor310575@gmail.com>
2026-01-24 19:11:12 -0500

    First Commit Part 2 (1 and 2)

--- part02-02_number_of_characters/src/number_of_characters.py
+++ part02-02_number_of_characters/src/number_of_characters.py
@@ -1,1 +1,6 @@
 # Write your solution here
+word = input("Please type in a word: ")
+word_len = len(word)
+if word != "b" and word_len >1:
+    print("There are " + str(word_len) + " letters in the word " + word)
+print("Thank you!")
