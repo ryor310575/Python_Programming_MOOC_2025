dae887f0d87a6e5e1192e08558e74c2f08de47d6
ryor31057 <ryor310575@gmail.com>
2026-01-26 01:07:30 -0500

    Third Commit Part 2 (8, 9, 10, 11, 12 and 13)

--- part02-10_grades_and_points/src/grades_and_points.py
+++ part02-10_grades_and_points/src/grades_and_points.py
@@ -1,1 +1,49 @@
+# The table below outlines the grade boundaries on a 
+# certain university course. Please write a program 
+# which asks for the amount of points received and 
+# then prints out the grade attained according to the table.
+
+
+# points	grade
+# < 0	impossible!
+# 0-49	fail
+# 50-59	1
+# 60-69	2
+# 70-79	3
+# 80-89	4
+# 90-100	5
+# > 100	impossible!
+
+# Some examples:
+
+# Sample output
+# How many points [0-100]: 37
+# Grade: fail
+
+# Sample output
+# How many points [0-100]: 76
+# Grade: 3
+
+# Sample output
+# How many points [0-100]: -3
+# Grade: impossible!
+
+
 # Write your solution here
+points = int(input("How many points [0-100]: "))
+if points < 0:
+    print("Grade: impossible!")
+elif points <= 49:
+    print("Grade: fail")
+elif points <= 59:
+    print("Grade: 1")
+elif points <= 69:
+    print("Grade: 2")
+elif points <= 79:
+    print("Grade: 3")
+elif points <= 89:
+    print("Grade: 4")
+elif points <= 100:
+    print("Grade: 5")
+else:
+    print("Grade: impossible!")
--- part02-11_fizzbuzz/src/fizzbuzz.py
+++ part02-11_fizzbuzz/src/fizzbuzz.py
@@ -1,1 +1,32 @@
+# Please write a program which asks the user for an 
+# integer number. If the number is divisible by three, 
+# the program should print out Fizz. If the number is 
+# divisible by five, the program should print out Buzz. 
+# If the number is divisible by both three and five, 
+# the program should print out FizzBuzz.
+
+# Some examples of expected behaviour:
+
+# Sample output
+# Number: 9
+# Fizz
+
+# Sample output
+# Number: 7
+
+# Sample output
+# Number: 20
+# Buzz
+
+# Sample output
+# Number: 45
+# FizzBuzz
+
 # Write your solution here
+num = int(input("Number: "))
+if num % 15 == 0 :
+    print("FizzBuzz")
+elif num % 3 == 0:
+    print("Fizz")
+elif num % 5 == 0:
+    print("Buzz")
