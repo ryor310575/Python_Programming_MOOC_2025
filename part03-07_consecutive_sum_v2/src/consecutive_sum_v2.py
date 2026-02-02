ca3a2ca5d13742d19ffe51f53fd02e7815d79f38
ryor31057 <ryor310575@gmail.com>
2026-02-01 19:07:44 -0500

    Commit From 3-4 to 3-10

--- part03-07_consecutive_sum_v2/src/consecutive_sum_v2.py
+++ part03-07_consecutive_sum_v2/src/consecutive_sum_v2.py
@@ -1,3 +1,20 @@
+# Please write a new version of the program in the previous exercise.
+# In addition to the result it should also print out the calculation performed:
+#
+# Sample output
+# Limit: 2
+# The consecutive sum: 1 + 2 = 3
+#
+# Sample output
+# Limit: 10
+# The consecutive sum: 1 + 2 + 3 + 4 = 10
+#
+# Sample output
+# Limit: 18
+# The consecutive sum: 1 + 2 + 3 + 4 + 5 + 6 = 21
+#
+# You may assume the number typed in by the user is always equal to 2 or higher.
+
 # Write your solution here
 limit=int(input("Limit: "))
 if limit<2:
--- part03-10_end_to_beginning/src/end_to_beginning.py
+++ part03-10_end_to_beginning/src/end_to_beginning.py
@@ -1,3 +1,16 @@
+# Please write a program which asks the user for a string.
+# The program then prints out the input string in reversed order,
+# from end to beginning. Each character should be on a separate line.
+#
+# An example of expected behaviour:
+#
+# Sample output
+# Please type in a string: hiya
+# a
+# y
+# i
+# h
+
 # Write your solution here
 word=input("Please type in a string: ")
 counter=len(word)
